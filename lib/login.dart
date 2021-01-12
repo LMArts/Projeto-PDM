5cf3100a427a49c5974762d0771e6836e23a7294
dlsdaniela <dls_daniela@live.com>
2021-01-11 23:50:56 -0300

    tela login

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
+import 'dart:developer';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
 class Login extends StatefulWidget {
   @override
@@ -8,42 +9,15 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-    TextEditingController emailResp  = TextEditingController();
-    TextEditingController senhaResp  = TextEditingController();
+   TextEditingController  email, senha;
 
-  Future login()async{
-      var url = 'http://localhost/loginPDM/login.php';
-      var response = await http.post(url, body:{       
-        "emailResp"  : emailResp.text,
-        "senhaResp"  : senhaResp.text,
-      });
 
-      var data = json.decode(response.body);
-      if(data == "Success"){
-        Fluttertoast.showToast(
-        msg: "Login realizado com sucesso!",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.green,
-        textColor: Colors.white,
-        fontSize: 16.0
-    );
-     Navigator.pushReplacementNamed(context, '/menu');
-      }else{
-        Fluttertoast.showToast(
-          msg: "Usuario ou senha incorreta!",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.CENTER,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0
-      );
-      }
+  @override
+    void initState() {
+      super.initState();
+      email = new TextEditingController();
+      senha = new TextEditingController();
     }
-
-
 
 
 
@@ -60,63 +34,73 @@ class _LoginState extends State<Login> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                width: 200,
-                height: 200,
+                width: 150,
+                height: 150,
                 child: Image.asset('assets/images/location.png'),
               ),
-              Container(height: 20,),
-              TextField(
-                controller: emailResp,
-                onChanged: (text) {
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: "E-mail", border: OutlineInputBorder()),
-              ),
-              SizedBox(
+              Container(
                 height: 10,
               ),
               TextField(
-                 controller: senhaResp,
-                onChanged: (text) {
-                },
+                controller: email,
+                decoration: InputDecoration(
+                    labelText: "email", border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextField(
+                controller: senha,
                 obscureText: true,
                 decoration: InputDecoration(
-                    labelText: "Senha", border: OutlineInputBorder()),
+                    labelText: "senha", border: OutlineInputBorder()),
               ),
               SizedBox(
-                height: 10,
+                height: 5,
               ),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/home');
-                  login();
+              MaterialButton(
+                onPressed: () => {
+                    loginResp(email.text, senha.text),
+                    
                 },
-                child: Text('Entrar'),
-                color: Colors.black,
-                textColor: Colors.white,
                 shape: new RoundedRectangleBorder(
                     borderRadius: new BorderRadius.circular(5)),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: GestureDetector(
-                  onTap: (){
-                    Navigator.pushReplacementNamed(context, '/cadResponsavel');
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Text(
-                      'Ainda não tenho cadastro!',
-                      style: TextStyle(color: Colors.grey, fontSize: 17.0),
-                    ),
-                  ),
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(color: Colors.white, fontSize: 17),
                 ),
+                color: Colors.black,
               ),
             ],
           ),
         ),
       ),
     ));
+  }
+   void loginResp(String email, String senha)async {
+    var url = "http://192.168.0.13/pdm/login.php";
+    var data ={
+      "email": email,
+      "senha": senha,
+    };
+    var res = http.post(url, body: data);
+   
+    print(res);
+    if(res == "Success"){
+
+      Navigator.pushReplacementNamed(context, '/home');
+    
+    }else{
+      Fluttertoast.showToast(
+        msg: "Login efetuado com sucesso!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+    Navigator.pushReplacementNamed(context, '/home');
+    }
   }
 }
